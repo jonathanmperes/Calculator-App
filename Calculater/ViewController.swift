da617afac8d85a25cf3122301dd34c3b70455c94
jonathanmarceloperes <emailmarceloperes@gmail.com>
2023-02-09 21:05:03 -0300

    Initial Commit

---
+++ Calculater/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Calculater
+//
+//  Created by Jonathan Marcelo Peres on 09/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Another message")
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func calculateTouched(_ sender: Any) {
+        print("Just a message")
+    }
+    
+}
+
