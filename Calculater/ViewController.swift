81ab9cd2f3f7673d76ec7f7ec3ae21af0485d561
jonathanmarceloperes <emailmarceloperes@gmail.com>
2023-02-17 15:13:33 -0300

    channge operation button color when clicked

--- Calculater/ViewController.swift
+++ Calculater/ViewController.swift
@@ -9,54 +9,82 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var firstNum: String = ""
+    var operation: String = ""
+    var secondNum: String = ""
+    var resultNum: String = "0"
+    
+    @IBOutlet var divideButton: UIButton!
+    @IBOutlet var multiplyButton: UIButton!
+    @IBOutlet var subtractButton: UIButton!
+    @IBOutlet var additionButton: UIButton!
+    
     @IBAction func divideButtonTapped(_ sender: Any) {
         operation = "/"
+        divideButton.backgroundColor = .purple
+        multiplyButton.backgroundColor = .systemBlue
+        subtractButton.backgroundColor = .systemBlue
+        additionButton.backgroundColor = .systemBlue
     }
     
     @IBAction func multiplyButtonTapped(_ sender: Any) {
         operation = "*"
+        divideButton.backgroundColor = .systemBlue
+        multiplyButton.backgroundColor = .purple
+        subtractButton.backgroundColor = .systemBlue
+        additionButton.backgroundColor = .systemBlue
     }
-    
     
     @IBAction func subtractButtonTapped(_ sender: Any) {
         operation = "-"
+        divideButton.backgroundColor = .systemBlue
+        multiplyButton.backgroundColor = .systemBlue
+        subtractButton.backgroundColor = .purple
+        additionButton.backgroundColor = .systemBlue
     }
     
     @IBAction func additionButtonTapped(_ sender: Any) {
         operation = "+"
+        divideButton.backgroundColor = .systemBlue
+        multiplyButton.backgroundColor = .systemBlue
+        subtractButton.backgroundColor = .systemBlue
+        additionButton.backgroundColor = .purple
     }
     
-    @IBOutlet weak var numOnScreen: UILabel!
-    @IBOutlet var textFieldOne: UITextField!
-    @IBOutlet var textFieldTwo: UITextField!
-    @IBOutlet var calculateButtonBottomConstraint: NSLayoutConstraint!
     
-    var firstNum = ""
-    var operation: String = ""
-    var secondNum = ""
-    var resultNum: String = "0"
+    @IBOutlet var numOnScreen: UILabel!
+    
+    @IBOutlet var textFieldOne: UITextField?
+    @IBOutlet var textFieldTwo: UITextField?
+    
+    @IBOutlet var calculateButtonBottomConstraint: NSLayoutConstraint!
     
     @IBAction func clearButtonTapped(_ sender: Any) {
         numOnScreen.text = "0"
-        textFieldOne.text = ""
-        textFieldTwo.text = ""
-        secondNum = ""
+        textFieldOne?.text = ""
+        textFieldTwo?.text = ""
+        divideButton.backgroundColor = .systemBlue
+        multiplyButton.backgroundColor = .systemBlue
+        subtractButton.backgroundColor = .systemBlue
+        additionButton.backgroundColor = .systemBlue
     }
     
     func calculate() -> Double {
-        let firstNumber = String(textFieldOne.text!)
-        firstNum = firstNumber
+        if let firstNumber = textFieldOne?.text! {
+            firstNum = firstNumber
+        }
         
-        let secondNumber = String(textFieldTwo.text!)
-        secondNum = secondNumber
+        if let secondNumber = textFieldTwo?.text! {
+            secondNum = secondNumber
+        }
         
-        if operation == "+" {
+        if operation == "+" && firstNum != "" && secondNum != "" {
             return Double(firstNum)! + Double(secondNum)!
-        } else if operation == "-" {
+        } else if operation == "-" && firstNum != "" && secondNum != "" {
             return Double(firstNum)! - Double(secondNum)!
-        } else if operation == "*"  {
+        } else if operation == "*" && firstNum != "" && secondNum != "" {
             return Double(firstNum)! * Double(secondNum)!
-        } else if operation == "/" {
+        } else if operation == "/" && firstNum != "" && secondNum != "" {
             return Double(firstNum)! / Double(secondNum)!
         }
         return 0
@@ -65,75 +93,20 @@ class ViewController: UIViewController {
     @IBAction func calculateButtonTapped(_ sender: Any) {
         view.endEditing(true)
         resultNum = String(calculate())
-        numOnScreen.text = resultNum
+        let numArray = resultNum.components(separatedBy: ".")
+        if numArray[1] == "0" {
+            numOnScreen.text = numArray[0]
+        } else {
+            numOnScreen.text = resultNum
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureObserverKeyboardMovement()
         configureTextFields()
         configureTapGesture()
     }
-    
-    private func configureObserverKeyboardMovement() {
-        NotificationCenter.default.addObserver(
-            self,
-            selector: #selector(self.keyboardWillShow),
-            name: UIResponder.keyboardWillShowNotification,
-            object: nil)
-
-        NotificationCenter.default.addObserver(
-            self,
-            selector: #selector(self.keyboardWillHide),
-            name: UIResponder.keyboardWillHideNotification,
-            object: nil)
-    }
-    
-    @objc func keyboardWillShow(_ notification: NSNotification) {
-        if textFieldOne.isEditing || textFieldTwo.isEditing {
-            moveViewWithKeyboard(
-                notification: notification,
-                viewBottomConstraint: self.calculateButtonBottomConstraint,
-                keyboardWillShow: true)
-        }
-    }
         
-    @objc func keyboardWillHide(_ notification: NSNotification) {
-        moveViewWithKeyboard(
-            notification: notification,
-            viewBottomConstraint: self.calculateButtonBottomConstraint,
-            keyboardWillShow: false)
-    }
-        
-    func moveViewWithKeyboard(
-        notification: NSNotification,
-        viewBottomConstraint: NSLayoutConstraint,
-        keyboardWillShow: Bool
-    ) {
-        guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else { return }
-        
-        let keyboardHeight = keyboardSize.height
-            
-        let keyboardDuration = notification.userInfo![UIResponder.keyboardAnimationDurationUserInfoKey] as! Double
-            
-        let keyboardCurve = UIView.AnimationCurve(rawValue: notification.userInfo![UIResponder.keyboardAnimationCurveUserInfoKey] as! Int)!
-            
-        if keyboardWillShow {
-            let safeAreaExists = (self.view?.window?.safeAreaInsets.bottom != 0)
-            let bottomConstant: CGFloat = 20
-            viewBottomConstraint.constant = keyboardHeight + (safeAreaExists ? 0 : bottomConstant)
-        } else {
-            viewBottomConstraint.constant = 20
-        }
-            
-        let animator = UIViewPropertyAnimator(
-            duration: keyboardDuration,
-            curve: keyboardCurve) { [weak self] in
-                self?.view.layoutIfNeeded()
-            }
-            animator.startAnimation()
-        }
-    
     private func configureTapGesture() {
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.handleTap))
         view.addGestureRecognizer(tapGesture)
@@ -144,8 +117,8 @@ class ViewController: UIViewController {
     }
         
     private func configureTextFields() {
-        textFieldOne.delegate = self
-        textFieldTwo.delegate = self
+        textFieldOne?.delegate = self
+        textFieldTwo?.delegate = self
     }
 }
 
